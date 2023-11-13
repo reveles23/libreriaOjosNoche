aa8cbd179ee97647e27f8f24f982b3a16cce9f5d
reveles23 <124103604+reveles23@users.noreply.github.com>
2023-11-13 10:51:02 -0600

    Add files via upload

---
+++ APILibreria/Controllers/AutorsController.cs
@@ -0,0 +1,26 @@
+﻿using LibreriaExamen2.Models;
+using LibreriaExamen2.Operaciones;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace APILibreria.Controllers
+{
+    [Route("api")]
+    [ApiController]
+    public class AutorsController : ControllerBase
+    {
+        private AutorDAO autorDAO = new AutorDAO();
+
+        [HttpPost("autor")]
+        public bool agregarAutor([FromBody] Autor autor)
+        {
+            return autorDAO.agregarAutor(autor.NombreAutor);
+        }
+
+        [HttpGet("autor")]
+        public List<Autor> obtenerAutors()
+        {
+            return autorDAO.obtenerAutors();
+        }
+    }
+}
---
+++ APILibreria/Controllers/LibrosController.cs
@@ -0,0 +1,38 @@
+﻿using LibreriaExamen2.Models;
+using LibreriaExamen2.Operaciones;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace APILibreria.Controllers
+{
+    [Route("api")]
+    [ApiController]
+    public class LibrosController : ControllerBase
+    {
+        private LibroDAO libroDAO = new LibroDAO();
+
+        [HttpPost("libro")]
+        public bool agregarLibro ([FromBody]Libro libro)
+        {
+            return libroDAO.agregarLibro(libro.IdAutor, libro.Titulo, libro.Capitulos, libro.Paginas, libro.Precio);
+        }
+
+        [HttpGet("libro")]
+        public List<Libro> obtenerLibro ()
+        {
+            return libroDAO.obtenerLibros();
+        }
+
+        [HttpGet("libroId")]
+        public Libro obtenerLibroId (int id)
+        {
+            return libroDAO.obtenerLibroId(id);
+        }
+
+        [HttpGet("LibroAutor")]
+        public List<LibroAutor> obtenerLibroAutor ()
+        {
+            return libroDAO.obtenerAutorLibros();
+        }
+    }
+}
